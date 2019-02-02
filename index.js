03968dab9f229b80ede747b4c56220fa37245287
Jennifer Gaillard <jengaillard1@gmail.com>
2019-02-02 02:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ if (Number.isInteger(currentAge)) {
 } else  {
 ageIsValid = false;
 }
-const randomNumber = Math.random()
-if random number = >0<20;
-
+const randomNumber = Math.random();
+if (random number) = > 0 < 20; {
+} 
 const randomInteger = Math.round(Math.random());
 const randomUserID = Math.round(Math.random() + 1000000001);
 
